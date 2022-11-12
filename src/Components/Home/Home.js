e8594e37f6dca61ef2ed24544cf0a6ad91ca2d3f
Eshitayasmin <eshita.emc2k17@gmail.com>
2022-11-12 21:41:42 +0600

    home page done

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,8 +1,13 @@
-import { faIndianRupeeSign } from '@fortawesome/free-solid-svg-icons';
+import { faIndianRupeeSign, faUsers } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import Navbar from '../Shared/Navbar';
 import './Home.css';
+import users from '../../images/users.png';
+import coupon from '../../images/coupon.png';
+import discount from '../../images/discount.png';
+import wallet from '../../images/wallet.png';
+import { Link } from 'react-router-dom';
 
 
 const Home = () => {
@@ -47,7 +52,64 @@ const Home = () => {
 
             </div>
 
+            <div className='how-work-div'>
+                <p className='how-work-p'>How does it work?</p>
+                <div className='works'>
+                     <div className='work'>
+                        <div className='icon-div'>
+                        <img className='users-icon' src={users} alt="" />
+                        </div>
+                        <div className='work-part2'>
+                            <h3>Invite your Friends</h3>
+                            <p>Share the link tutedude.com with your friends</p>
+                            
+                        </div>
+                     </div>
+                     <div className='work'>
+                        <div className='icon-div'>
+                        <img className='coupon-icon' src={coupon} alt="" />
+                        </div>
+                        <div className='work-part2'>
+                            <h3>Friends purchases any course</h3>
+                            <p>Using REFERRAL CODE in the payments page</p>
+                            
+                        </div>
+                     </div>
+                     <div className='work'>
+                        <div className='icon-div'><p className='rupee-icon'>₹</p></div>
+                        <div className='work-part2'>
+                            <h3>You got ₹200 as referral money</h3>
+                            <p>On total purchase the friend makes, into your wallet.</p>
+                            
+                        </div>
+                     </div>
+                     <div className='work'>
+                        <div className='icon-div'>
+                        <img className='discount-icon' src={discount} alt="" />
+                        </div>
+                        <div className='work-part2'>
+                            <h3>Your Friend gets ₹200 off</h3>
+                            <p>On his overall fee on successful purchase using your referral code</p>
+                            
+                        </div>
+                     </div>
+                     <div className='work'>
+                        <div className='icon-div'>
+                        <img className='wallet-icon' src={wallet} alt="" />
+                        </div>
+                        <div className='work-part2'>
+                            <h3>Transfer money from wallet</h3>
+                            <p>When the wallet balance reaches ₹200 or more, you can withdraw it</p>
+                            
+                        </div>
+                     </div>
+                </div>
+            </div>
 
+          <div className='last-part'>
+            <p>Friends who Enrolled</p>
+            <p>Terms & Conditions</p>
+          </div>
         </div>
     );
 };
